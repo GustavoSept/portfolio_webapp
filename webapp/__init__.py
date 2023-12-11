1a1ef74d9bfbddef5c1c29ddde9413414a3d73ac
GustavoSept <GustavoSept.Cron@gmail.com>
2023-12-11 12:37:06 -0300

    including compoundInterest in init

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,3 +6,7 @@ application.secret_key = 'your_secret_key'
 # needs to come after app instance is created
 from webapp import routes
 from webapp import portfolioProjection
+from webapp import compoundInterest
+
+# for rule in application.url_map.iter_rules():
+#     print(rule)
