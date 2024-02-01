6efc3df77cdc94be948de19257188df689493512
GustavoSept <GustavoSept.Cron@gmail.com>
2024-02-01 17:38:07 -0300

    putting css into a separate file

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-application = Flask(__name__)
+application = Flask(__name__, static_url_path='/static')
 application.secret_key = 'your_secret_key'
 
 # needs to come after app instance is created
