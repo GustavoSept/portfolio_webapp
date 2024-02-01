3998852e4e6d77cfaad280312b99f506f01d203e
GustavoSept <GustavoSept.Cron@gmail.com>
2024-02-01 17:33:07 -0300

    setting debug mode off in production

--- application.py
+++ application.py
@@ -2,6 +2,8 @@ from webapp import application
 from webapp.data_fetcher import start_data_fetcher
 
 if __name__ == '__main__':
-    application.run(debug=False, host='0.0.0.0', port=5000)
     # Start the data fetcher
-    start_data_fetcher(interval=15)  # Set to A LOW VALUE for TESTING
+    start_data_fetcher()
+
+    # Start the web application
+    application.run(debug=False, host='0.0.0.0', port=5000)
