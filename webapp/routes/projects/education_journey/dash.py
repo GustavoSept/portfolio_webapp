ddfa0674013b2a86eca274fd5c8f56ae6a11566a
GustavoSept <GustavoSept.Cron@gmail.com>
2024-08-11 18:33:36 -0300

    WIP: using sqlite as a cache

--- webapp/routes/projects/education_journey/dash.py
+++ webapp/routes/projects/education_journey/dash.py
@@ -1,9 +1,37 @@
+import os
+import sqlite3
 from dash import Dash, dcc, html
 
 import pandas as pd
 import dash_bootstrap_components as dbc
 
 from .callbacks import DashCallbacks
+from webapp.helpers.db import should_fetch_df, save_df_to_sqlite
+
+def fetch_and_process_data() -> pd.DataFrame:
+    url = 'https://docs.google.com/spreadsheets/d/17_Eq4kJ6LE4hVF-kaa6P6YOy07bukCtQuMHYcNYLjWc/export?format=csv'
+    df = pd.read_csv(url)
+    df = df[df['Specific Content'].notna()] # deleting rows with NaN values
+    df = preprocess_labels(df, 'Label')
+    save_df_to_sqlite(df)
+    return df
+
+def get_data_from_sqlite() -> pd.DataFrame:
+    BASE_DIR = os.getenv('DB_BASE_DIR')
+    db_path = os.path.join(BASE_DIR, 'education_journey.db')
+    with sqlite3.connect(db_path) as conn:
+        df = pd.read_sql_query("SELECT * FROM education_journey", conn)
+    return df
+
+def preprocess_labels(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    """Preprocesses the labels in the specified column by appending numeric suffixes to duplicate labels"""
+    label_counts = df[column_name].value_counts()
+    for label, count in label_counts.items():
+        if count > 1:
+            label_rows = df[df[column_name] == label]
+            new_labels = [f"{i+1}_{label}" for i in range(count)]
+            df.loc[label_rows.index, column_name] = new_labels
+    return df
 
 def dash_educational_journey(flask_app):
     dash_app = Dash(
@@ -13,36 +41,15 @@ def dash_educational_journey(flask_app):
             routes_pathname_prefix="/dash/educationJourney/",
             suppress_callback_exceptions=True,
             )
-    
-    # Load data from the Google Sheets URL
-    url = 'https://docs.google.com/spreadsheets/d/17_Eq4kJ6LE4hVF-kaa6P6YOy07bukCtQuMHYcNYLjWc/export?format=csv'
-    df = pd.read_csv(url)
 
-    # deleting rows with NaN values
-    df = df[df['Specific Content'].notna()]
+
+    if should_fetch_df():
+        df = fetch_and_process_data()
+    else:
+        df = get_data_from_sqlite()
 
     HOURS_STUDIED = int(df['Time (in hours)'].sum())
     UNIQUE_INSTITUTIONS = df['Institution'].unique()
-
-    # Preprocessing the 'Label' column, to mantain divisions
-    def preprocess_labels(df, column_name):
-        label_counts = df[column_name].value_counts()
-        
-        # For labels that occur more than once, add a prefix
-        for label, count in label_counts.items():
-            if count > 1:
-                # Filter rows with the current label
-                label_rows = df[df[column_name] == label]
-                
-                # Generate new labels with prefixes
-                new_labels = [f"{i+1}_{label}" for i in range(count)]
-                
-                df.loc[label_rows.index, column_name] = new_labels
-
-        return df
-
-    df = preprocess_labels(df, 'Label')
-
 
     # checklist to filter data between institutions
     checklist = dcc.Checklist(
